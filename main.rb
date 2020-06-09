79e31bd6dcc729e7cee62f0459b6b104a9ffa31e
YuichiTanaka <smile.yuichi.1013@gmail.com>
2020-06-09 21:54:55 +0900

    add task2 - add attack method

--- main.rb
+++ main.rb
@@ -4,10 +4,12 @@ require "./monster.rb"
 brave = Brave.new(hp:500, offense:150, defense:100)
 monster = Monster.new(hp:300, offense:200, defense:300)
 
-puts brave.hp
-puts brave.offense
-puts brave.defense
-puts monster.hp
-puts monster.offense
-puts monster.defense
+# puts brave.hp
+# puts brave.offense
+# puts brave.defense
+# puts monster.hp
+# puts monster.offense
+# puts monster.defense
 
+brave.attack(monster)
+monster.attack(brave)
