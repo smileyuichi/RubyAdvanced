fa0d1dea31af700426df04e4dbaa0f8cc90995c6
YuichiTanaka <smile.yuichi.1013@gmail.com>
2020-06-10 12:43:29 +0900

    add task3

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require "./monster.rb"
 brave = Brave.new(hp:500, offense:150, defense:100)
 monster = Monster.new(hp:300, offense:200, defense:300)
 
-どちらかのHPが０になったらゲーム終了
+# どちらかのHPが０になったらゲーム終了
 loop do
     brave.attack(monster)
     break if monster.hp <= 0
